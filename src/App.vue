0cf74edc9bc9f181ebfd8e4a042285e520191f9b
DIMONZ23 <ducthang28122003>
2024-08-24 17:12:15 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+const message = 'Hello im Dimonz!'
+const rawHtml = '<span style="color:red;">Im so sorry!</span>'
+const buttonId = 'button-id'
+const buttonClass = 'button-class-rounded'
+const isButtonDisabled = true
+const objectOfAttrs = {
+  id: 'container-id',
+  class: 'wrapper-class',
+  style: {
+    color: 'red'
+  }
+}
+</script>
+
+<template>
+  <h1>Syntax</h1>
+  <p>Message: {{ message }}</p>
+  <span v-html="rawHtml"></span>
+  <button :id="buttonId" :class="buttonClass" :disabled="isButtonDisabled">Click here</button>
+  <button v-bind="objectOfAttrs">Click now</button>
+</template>
