24c219a5cd620b4399d5c7f52e3a81bcb92e2fd2
DIMONZ23 <ducthang28122003@gmail.com>
2024-08-29 13:35:21 +0700

    [Aug-W04][FE] Biding Data

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ const message = 'Hello im Dimonz!'
 const rawHtml = '<span style="color:red;">Im so sorry!</span>'
 const buttonId = 'button-id'
 const buttonClass = 'button-class-rounded'
-const isButtonDisabled = true
+const isButtonDisabled = false
 const objectOfAttrs = {
   id: 'container-id',
   class: 'wrapper-class',
@@ -11,14 +11,19 @@ const objectOfAttrs = {
     color: 'red'
   }
 }
+const number = 2;
+const listString = 'a,b,c,d,e,f';
+const convertToId = (data) => data.split(',').reverse().join('-')
 </script>
 
 <template>
   <h1 class="center-text">Syntax</h1>
   <p>Message: {{ message }}</p>
   <span v-html="rawHtml"></span>
-  <button :id="buttonId" :class="buttonClass" :disabled="isButtonDisabled">Click here</button>
+  <button :id="buttonId" :class="buttonClass" :disabled="isButtonDisabled">{{isButtonDisabled?'Disable':'Click Here'}}</button>
   <button v-bind="objectOfAttrs">Click now</button>
+  <button :id="convertToId(listString)"> Number:{{ number *2 }}</button>
+  
 </template>
 <style>
 .center-text {
